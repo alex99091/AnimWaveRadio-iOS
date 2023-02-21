900f342a9e55d63f690d979eee8af9134cc01776
alex99091 <bkk99091@gmail.com>
2023-02-22 04:29:40 +0900

    design update (3)

--- AnimWaveRadio-iOS/ViewController/RadioViewController.swift
+++ AnimWaveRadio-iOS/ViewController/RadioViewController.swift
@@ -25,19 +25,20 @@ class RadioViewController: UIViewController {
     let audioPlayer = AVPlayer()
     var locationManger = CLLocationManager()
     var frequencyValue: Double = 0.0
+    var playStatus: Bool = false
     
     // Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadingAnimeView()
         //findLocation()
         setUpCircularSliderIcon()
         setUpRadioRange()
         
-        loadingAnimeView()
     }
     
     // loading Animation
-    // 각각의 뷰를 시간에 맞추어 변하게하는 뷰
+    // 각각의 뷰를 애니매이션 동작으로 로딩하는 함수
     func loadingAnimeView() {
         let maxWidth = self.animeView.frame.width
         let maxHeight = self.animeView.frame.height
@@ -50,12 +51,18 @@ class RadioViewController: UIViewController {
         let inSquarelineView4 = UIView()
         let inSquarelineView5 = UIView()
         let circleView = UIView()
+        let inCirlcelineView = UIView()
+        let inCirlcelineView2 = UIView()
+        let inCirlcelineView3 = UIView()
+        let inCirlcelineView4 = UIView()
+        let topRectangleView = UIView()
+        let longRectangleView = UIView()
         
         var startX: Double = 0
         var startY: Double = 0
         var count: CGFloat = 0
         
-        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { timer in
+        Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { timer in
             self.animeView.addSubview(radioRectangleView)
             if count <= 10 {
                 startX = maxWidth * (0.5 - 0.09/2)
@@ -121,96 +128,153 @@ class RadioViewController: UIViewController {
             self.animeView.addSubview(inSquarelineView2)
             if count == 33 {
                 inSquarelineView2.frame = CGRect(x: maxWidth * (0.06 + 0.058),
-                                                y: maxHeight * (0.475 + 0.069),
-                                                width: maxWidth * 0.3,
-                                                height: 10.0)
+                                                 y: maxHeight * (0.475 + 0.069),
+                                                 width: maxWidth * 0.3,
+                                                 height: 10.0)
                 inSquarelineView2.layer.cornerRadius = 5.0
                 inSquarelineView2.layer.borderWidth = 3.0
             }
             self.animeView.addSubview(inSquarelineView3)
             if count == 34 {
                 inSquarelineView3.frame = CGRect(x: maxWidth * (0.06 + 0.058),
-                                                y: maxHeight * (0.544 + 0.069),
-                                                width: maxWidth * 0.3,
-                                                height: 10.0)
+                                                 y: maxHeight * (0.544 + 0.069),
+                                                 width: maxWidth * 0.3,
+                                                 height: 10.0)
                 inSquarelineView3.layer.cornerRadius = 5.0
                 inSquarelineView3.layer.borderWidth = 3.0
             }
             self.animeView.addSubview(inSquarelineView4)
             if count == 35 {
                 inSquarelineView4.frame = CGRect(x: maxWidth * (0.06 + 0.058),
-                                                y: maxHeight * (0.613 + 0.069),
-                                                width: maxWidth * 0.3,
-                                                height: 10.0)
+                                                 y: maxHeight * (0.613 + 0.069),
+                                                 width: maxWidth * 0.3,
+                                                 height: 10.0)
                 inSquarelineView4.layer.cornerRadius = 5.0
                 inSquarelineView4.layer.borderWidth = 3.0
             }
             self.animeView.addSubview(inSquarelineView5)
             if count == 36 {
                 inSquarelineView5.frame = CGRect(x: maxWidth * (0.06 + 0.058),
-                                                y: maxHeight * (0.682 + 0.069),
-                                                width: maxWidth * 0.3,
-                                                height: 10.0)
+                                                 y: maxHeight * (0.682 + 0.069),
+                                                 width: maxWidth * 0.3,
+                                                 height: 10.0)
                 inSquarelineView5.layer.cornerRadius = 5.0
                 inSquarelineView5.layer.borderWidth = 3.0
             }
             self.animeView.addSubview(circleView)
-            if count > 36 && count <= 45 {
+            if count > 36 && count < 45 {
+                circleView.frame = CGRect(x: maxWidth * 0.52,
+                                          y: maxHeight * 0.416,
+                                          width: maxWidth * 0.33/9 * (count - 35),
+                                          height: maxHeight * 0.416/9 * (count - 35))
+                circleView.layer.cornerRadius = (maxWidth * 0.33/9 * (count - 35)) / 2
+                circleView.layer.borderWidth = 5.0
+                circleView.layer.borderColor = UIColor.black.cgColor
+            }
+            if count == 45 {
                 circleView.frame = CGRect(x: maxWidth * 0.52,
                                           y: maxHeight * 0.416,
                                           width: maxWidth * 0.33,
                                           height: maxHeight * 0.416)
                 circleView.layer.cornerRadius = maxWidth * 0.33 / 2
-                circleView.layer.borderWidth = 5.0
-                circleView.layer.borderColor = UIColor.black.cgColor
-                circleView.backgroundColor = UIColor.white
             }
-            
-            
+            self.animeView.addSubview(inCirlcelineView)
+            if count == 46 {
+                inCirlcelineView.frame = CGRect(x: maxWidth * (0.52 + 0.06),
+                                                y: maxHeight * 0.485,
+                                                width: maxWidth * 0.21,
+                                                height: maxHeight * 0.049)
+                inCirlcelineView.layer.cornerRadius = 10.0
+                inCirlcelineView.layer.borderWidth = 3.0
+            }
+            self.animeView.addSubview(inCirlcelineView2)
+            if count == 47 {
+                inCirlcelineView2.frame = CGRect(x: maxWidth * (0.52 + 0.06),
+                                                 y: maxHeight * 0.559,
+                                                 width: maxWidth * 0.21,
+                                                 height: maxHeight * 0.049)
+                inCirlcelineView2.layer.cornerRadius = 10.0
+                inCirlcelineView2.layer.borderWidth = 3.0
+            }
+            self.animeView.addSubview(inCirlcelineView3)
+            if count == 48 {
+                inCirlcelineView3.frame = CGRect(x: maxWidth * (0.52 + 0.06),
+                                                 y: maxHeight * 0.633,
+                                                 width: maxWidth * 0.21,
+                                                 height: maxHeight * 0.049)
+                inCirlcelineView3.layer.cornerRadius = 10.0
+                inCirlcelineView3.layer.borderWidth = 3.0
+            }
+            self.animeView.addSubview(inCirlcelineView4)
+            if count == 49 {
+                inCirlcelineView4.frame = CGRect(x: maxWidth * (0.52 + 0.06),
+                                                 y: maxHeight * 0.707,
+                                                 width: maxWidth * 0.21,
+                                                 height: maxHeight * 0.049)
+                inCirlcelineView4.layer.cornerRadius = 10.0
+                inCirlcelineView4.layer.borderWidth = 3.0
+            }
+            self.animeView.addSubview(topRectangleView)
+            if count > 49 && count < 55 {
+                topRectangleView.frame = CGRect(x: maxWidth * 0.06,
+                                                y: maxHeight * 0.386 - 6.0 * (count - 49),
+                                                width: 10.0 * (count - 49),
+                                                height: 6.0 * (count - 49))
+                topRectangleView.layer.cornerRadius = 12.5
+                topRectangleView.layer.borderWidth = 5.0
+                topRectangleView.backgroundColor = UIColor.white
+            }
+            self.animeView.addSubview(longRectangleView)
+            if count >= 55 && count < 60 {
+                longRectangleView.frame = CGRect(x: maxWidth * 0.06 + 15,
+                                                 y: maxHeight * 0.386 - 27.5 - 18.0 * (count - 54),
+                                                 width: 3.0 * (count - 54),
+                                                 height: 18.0 * (count - 54))
+                longRectangleView.layer.cornerRadius = 6.25
+                longRectangleView.layer.borderWidth = 5.0
+                longRectangleView.backgroundColor = UIColor.white
+            }
             count += 1
-            if count == 45 {
+            if count == 60 {
                 timer.invalidate()
             }
         })
     }
     
-    
-    
-    
-    func loadingAnimeView2(){
-        // minWidth ~ MaxWidth로 Variation
-        let circleMinWidth: CGFloat = 5.0
-        let circleMaxWidth: CGFloat = 25.0
-        // starting (x,y)
-        let startX = animeView.frame.width / 2
-        let startY = animeView.frame.height / 2
-        let loadingView = UIView()
-        animeView.addSubview(loadingView)
-        
-        // (x,y 좌표 변화값)
-        var i: Int = 0
-        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { timer in
-            
-            let changeX: Double = sin( (Double(i) * 15) * M_PI / 180 )
-            let changeY: Double = cos( (Double(i) * 15) * M_PI / 180 )
-            let currentWidthAndHeight = circleMinWidth + (circleMaxWidth - circleMinWidth) * CGFloat(i) / 24
-            let radius = self.animeView.frame.width / 4
-            
-            loadingView.frame = CGRect(x: startX + CGFloat(changeX) * radius - currentWidthAndHeight,
-                                       y: startY - CGFloat(changeY) * radius - currentWidthAndHeight,
-                                       width: currentWidthAndHeight,
-                                       height: currentWidthAndHeight)
-            loadingView.layer.cornerRadius = currentWidthAndHeight / 2
-            loadingView.layer.borderWidth = 1.5
-            loadingView.layer.borderColor = UIColor.black.cgColor
-            loadingView.backgroundColor = UIColor.white
-            
-            i += 1
-            if i > 24 && i <= 48{
-                
-                timer.invalidate()
-            }
-        })
+    // PlayButton이 터치되면 AnimeView를 동작하는 함수
+    func radioPlayStart() {
+        if playStatus == true {
+            print("radioPlay Activated")
+            var count = 0
+            Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true, block: { timer in
+                var startX = self.animeView.frame.width * 0.06 + 40
+                var startY = (self.animeView.frame.height * 0.416) / 2
+                for i in 0...7 {
+                    let waveHalfCircle = UIView()
+                    let waveRectangle = UIView()
+                    let width = CGFloat(10.0 * Double(i))
+                    let height = CGFloat(10.0 * Double(i))
+                    startX += width
+                    waveHalfCircle.frame = CGRect(x: startX ,
+                                                  y: startY - height / 2,
+                                                  width: width,
+                                                  height: height)
+                    waveHalfCircle.layer.cornerRadius = width / 2
+                    waveHalfCircle.layer.borderWidth = 5.0
+                    waveHalfCircle.backgroundColor = UIColor.white
+                    self.animeView.addSubview(waveHalfCircle)
+                    waveRectangle.frame = CGRect(x: startX,
+                                                 y: startY - height / 2,
+                                                 width: width / 2,
+                                                 height: height)
+                    waveRectangle.backgroundColor = UIColor.white
+                    self.animeView.addSubview(waveRectangle)
+                }
+                count += 1
+                count %= 7
+                print(count)
+            })
+        }
     }
     
     // Method
@@ -230,5 +294,15 @@ class RadioViewController: UIViewController {
         frequencyValue = frequencyValue == 0 ? 100 : frequencyValue
         valueLabel.text = String(format: "%.1f"+"MHz", frequencyValue)
     }
+    
+    // IBAction
+    @IBAction func playButtonTouched(_ sender: Any) {
+        print("play Button Touched")
+        playStatus = true
+        radioPlayStart()
+        
+    }
+    
+    
 }
 
